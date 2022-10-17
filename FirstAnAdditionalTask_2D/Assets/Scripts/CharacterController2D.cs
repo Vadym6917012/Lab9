05b66964c8c28854240dcd3c136485445c4469fe
Vadym6917012 <vadimr69170@gmail.com>
2022-10-17 17:57:35 +0300

    Update Animations

--- FirstAnAdditionalTask_2D/Assets/Scripts/CharacterController2D.cs
+++ FirstAnAdditionalTask_2D/Assets/Scripts/CharacterController2D.cs
@@ -6,42 +6,21 @@ public class CharacterController2D : MonoBehaviour
     public float playerSpeed = 5f;
     public float playerHealth = 100f;
 
-    [Space]
-    [Header("Character sprites")]
-    public Sprite playerFront;
-    public Sprite playerBack;
-    public Sprite playerLeft;
-    public Sprite playerRight;
-
     private Vector2 _movementVector;
 
     private Rigidbody2D _rbody;
     private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
 
     private void Start()
     {
         _rbody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            _spriteRenderer.sprite = playerBack;
-        }
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            _spriteRenderer.sprite = playerFront;
-        } 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            _spriteRenderer.sprite = playerRight;
-        } 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            _spriteRenderer.sprite = playerLeft;
-        }
         Move();
     }
 
@@ -49,6 +28,10 @@ public class CharacterController2D : MonoBehaviour
     {
         var horizontalInput = Input.GetAxis("Horizontal");
         var verticalInput = Input.GetAxis("Vertical");
+
+        _animator.SetFloat("Horizontal", _movementVector.x);
+        _animator.SetFloat("Vertical", _movementVector.y);
+        _animator.SetFloat("Speed", _movementVector.sqrMagnitude);
 
         _movementVector = new Vector2(horizontalInput * playerSpeed, verticalInput * playerSpeed);
         _rbody.velocity = _movementVector;
